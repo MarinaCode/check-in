d7a0e0e076bbf26dbe209042c90094894d9bf396
Marina <marinakhanamiryan@gmail.com>
2017-05-31 20:31:21 +0400

    fix

--- README.md
+++
@@ -1,2 +0,0 @@
-# check-in
-check-in
--- gulpfile.js
+++
@@ -1,9 +0,0 @@
-var gulp = require('gulp');
-var exec = require('child_process').exec;
-gulp.task('build', function (cb) {
-  exec('ng build',{maxBuffer: 1024 * 500}, function (err, stdout, stderr) {
-    console.log(stdout);
-    console.log(stderr);
-    cb(err);
-  });
-})
